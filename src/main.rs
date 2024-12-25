dd8426f772568154a4c5628bc14988e9c59a1f28
Uglypr1nces <shpatavdiu4@gmail.com>
2024-12-25 22:55:31 +0100

    guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,39 @@
-//main function
+//Guessing game
+use rand::Rng;
+use std::io;
+use std::any::type_name;
 
 fn main() {
+    println!("Hello, seletct a number between 1-10!");
+    let mut guess = String::new();
 
-    println!("Hello, world!");
-    println!("My name is {}", "shpat")
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    
+    let string_slice: &str = &guess;
+    let int_guess = string_slice.parse::<i32>();
+
+    println!("{}", type_of(int_guess));
+
+
+    //checkNum(generateNum(), int_guess);
+}
+
+fn generateNum() -> i32{
+    let mut rng = rand::thread_rng();
+    let random_number: i32 = rng.gen_range(0..10);
+    return random_number;
+}
+
+fn checkNum(x: i32, y: i32){
+    if x == y{
+        println!("you won")
+    } else {
+        println!("wrong guess")
+    }
+}
+
+fn type_of<T>(_: T) -> &'static str {
+    type_name::<T>()
 }
